b8b8972bbf607dc82f3a2ef184efa694b7d3abc0
jasper-springael <56021344+jasper-springael@users.noreply.github.com>
2019-12-03 19:31:35 +0100

    added extra css and dynamic divs

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
       curl_setopt_array($curl, array(
 
-        CURLOPT_URL => "https://api.openweathermap.org/data/2.5/forecast/?q=".$city."&appid=".$key,
+        CURLOPT_URL => "https://api.openweathermap.org/data/2.5/forecast/?q=".$city."&appid=".$key."&units=metric",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_TIMEOUT => 30,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
@@ -23,20 +23,45 @@
 
       $message=$result['message'];
 
-      if ($message ==null && $message ==0) {
+      if ($message ==null) {
         $length=count($result['list']);
         ChromePhp::log($length);
         ChromePhp::log($result['list'][0]['main']['temp']);
-        for($i=0;$i<$length;$i+=8) {
-          echo("<div class=\"results\">");
-          echo($result['list'][$i]['main']['temp'].' ');
-          echo($result['list'][$i]['main']['temp_min'].' ');
-          echo($result['list'][$i]['main']['temp_max'].' ');
-          echo($result['list'][$i]['main']['humidity'].' ');
-          echo($result['list'][$i]['weather'][0]['icon'].' ');
-          echo($result['list'][$i]['weather'][0]['description'].' ');
-          echo($result['list'][$i]['wind']['speed'].' ');
-          echo("</div>");
+        $minTemps = array ();
+        $maxTemps = array ();
+        for ($i=0;$i<8;$i++) {
+          array_push($minTemps,$result['list'][$i]['main']['temp_min']);
+          array_push($maxTemps,$result['list'][$i]['main']['temp_max']);
+        }
+        ChromePhp::log($minTemps);
+        ChromePhp::log($maxTemps);
+        $icons= $result['list'][$i]['weather'][0]['icon'];
+        // div 1
+        echo("<div class=\"first-result-container\">");
+        echo("<div class=\"first-result-1a\">");
+        echo($result['list'][0]['main']['temp']);
+        echo("</div>");
+        echo("<div class=\"first-result-1b\">");
+        echo("<img src=http://openweathermap.org/img/w/".$icons.".png></img>");
+        echo($result['list'][0]['weather'][0]['description'].' ');
+        echo("</div>");
+        echo("</div>");
+        // einde div 1
+        echo("<div class=\"first-result-2\">");
+        echo(max($maxTemps));
+        echo(min($minTemps));
+        echo($result['list'][0]['main']['humidity'].' ');
+        echo($result['list'][0]['wind']['speed'].' ');
+        echo("</div>");
+        for($i=8;$i<$length;$i+=8) {
+        echo("<div class=\"results\">");
+        echo($result['list'][$i]['main']['temp'].' ');
+        echo($result['list'][$i]['main']['temp_min']);
+        echo($result['list'][$i]['main']['temp_max']);
+        echo($result['list'][$i]['weather'][0]['description'].' ');
+        $icons= $result['list'][$i]['weather'][0]['icon'];
+        echo("<img src=http://openweathermap.org/img/w/".$icons.".png></img>");
+        echo("</div>");
         }
       } else { 
         echo("<div class=\"results\">");
@@ -52,24 +77,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 <div class="container">
-  <form action="" method="POST">
-    <input type="text" name="text">
-    <input type="submit" name="submit">
-  </form>
-  <div id="wrapper">
-    <?php
-    if (isset($_POST['submit'])) {
-      getData();
-    };
-    ?>
+  <div class="top-side-container">
+    <div class="top-side">
+      <form action="" method="POST">
+        <input type="text" name="text">
+        <input type="submit" name="submit">
+      </form>
+    </div>
+    <div class="top-side-2">
+    <h2>Gent</h2>
+    </div>
   </div>
+      <?php
+      if (isset($_POST['submit'])) {
+        getData();
+      };
+      ?>
 </div>
 </body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</html>
+</html> 
